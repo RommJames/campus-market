46ee5f16fb5557af9a3616257697e21aae64140d
sofiaaloran <aloransofia26@gmail.com>
2026-07-15 15:35:43 +0800

    upload front end

---
+++ includes/footer.php
@@ -0,0 +1,27 @@
+<footer class="py-5 mt-5 bg-white border-top">
+
+<div class="container text-center">
+
+<h5 class="fw-bold">
+
+CampusMarket
+
+</h5>
+
+<p class="text-secondary">
+
+A marketplace exclusively for CIIT students.
+
+</p>
+
+<p class="small text-muted">
+
+© 2026 CampusMarket.
+
+All Rights Reserved.
+
+</p>
+
+</div>
+
+</footer>
---
+++ login.php
@@ -0,0 +1,99 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CampusMarket | Login</title>
+
+    <!-- Bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css" rel="stylesheet">
+
+    <link rel="stylesheet"
+    href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+    
+    <!-- Google Font -->
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet">
+
+    <!-- Custom CSS -->
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="assets/css/login.css">
+</head>
+
+<body>
+
+<div class="login-wrapper">
+
+    <!-- Left Panel -->
+    <div class="login-left">
+
+        <h1>CampusMarket</h1>
+
+        <p>
+            Buy, Sell, and Exchange
+            with fellow CIIT students.
+        </p>
+
+
+    </div>
+
+    <!-- Right Panel -->
+    <div class="login-right">
+
+        <div class="login-card">
+
+            <h2>Welcome Back</h2>
+
+            <p>Login to continue.</p>
+
+            <form>
+
+                <div class="mb-3">
+
+                    <label>Email</label>
+
+                    <input
+                    type="email"
+                    class="form-control"
+                    placeholder="Enter your email">
+
+                </div>
+
+                <div class="mb-4">
+
+                    <label>Password</label>
+
+                    <input
+                    type="password"
+                    class="form-control"
+                    placeholder="Enter your password">
+
+                </div>
+
+                <button class="btn btn-primary-custom w-100">
+
+                    Login
+
+                </button>
+
+            </form>
+
+            <div class="text-center mt-4">
+
+                Don't have an account?
+
+                <a href="register.php">
+
+                    Register
+
+                </a>
+
+            </div>
+
+        </div>
+
+    </div>
+
+</div>
+
+</body>
+</html>
